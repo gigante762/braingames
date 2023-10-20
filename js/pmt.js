5f864605d7efe3e81311bf58b971d3860c18c4ce
Kevin Souza <kevin.souza@devsquad.com>
2023-10-20 20:14:06 -0300

    have it working

--- js/pmt.js
+++ js/pmt.js
@@ -9,11 +9,37 @@ function pmtIni() {
     const r = this.tax / 100; 
     const PMT = this.loan * r / (1 - Math.pow(1 + r, -this.months));
 
-    return parseFloat(PMT.toFixed(3));
+    return parseFloat(PMT.toFixed(this.decimals));
    },
 
    get finalValue() {
+    console.log(this.pmt * this.months)
     return this.pmt * this.months
+   },
+
+   get gerarPreview(){
+    data = []
+    let finalValue = this.loan;
+  
+    for (let i = 0; finalValue > 0; i++) {
+      pagou = i==0 ? 0: this.pmt;
+      tem_que_pagar = parseFloat((finalValue - pagou).toFixed(this.decimals))
+      tem_que_pagar_com_juros = parseFloat(((tem_que_pagar + tem_que_pagar*(this.tax/100))).toFixed(this.decimals))
+      
+      item = {
+        mes: i,
+        pagou: pagou,
+        tem_que_pagar: tem_que_pagar,
+        tem_que_pagar_com_juros: tem_que_pagar_com_juros
+      };
+      data.push(item);
+
+      console.log(item)
+
+      finalValue =  tem_que_pagar_com_juros;
+    }
+
+    return data;
    },
 
     init() {
